1ed7db728b0cc131a2600af46d05c63c78ce3868
dangchien <82793130+vdchien98@users.noreply.github.com>
2022-08-02 01:04:26 +0700

    password -decryptPassword

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const PORT = 3001;
+const cors = require('cors');
+
+const { encrypt, decrypt } = require('./EncryptionHandler');
+app.use(cors());
+app.use(express.json());
+const db = mysql.createConnection({
+    user: 'root',
+    hostL: 'localhost',
+    password: '09041998',
+    database: 'sern2',
+    port: 3307,
+});
+
+app.post('/addpassword', (req, res) => {
+    const { password, title } = req.body;
+    const hashedPassword = encrypt(password);
+    db.query('INSERT INTO passwords (password, title, iv) VALUES (?,?,?)', [hashedPassword.password, title, hashedPassword.iv], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send('Success');
+        }
+    });
+});
+
+app.get('/showpasswords', (req, res) => {
+    db.query('SELECT * FROM passwords;', (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.post('/decryptpassword', (req, res) => {
+    res.send(decrypt(req.body));
+});
+app.listen(PORT, () => {
+    console.log('Server is running');
+});
